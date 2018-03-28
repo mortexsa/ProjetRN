c2b3edd3344c8d5fbf5b0cda13198bbf446cfd9c
pepnou <pepnou1@gmail.com>
2018-03-28 19:41:51 +0200

    allez y testez c est marrant pour compiler: gcc -lpthread -Wall -o test test.c pour lancer: ./test

--- test_perf_multVM/test.c
+++ test_perf_multVM/test.c
@@ -1,9 +1,36 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <pthread.h>
+
+#define PTHREAD_THREADS_MAX 1024
 
 int w = 10000;
 int h = 10000;
+
+typedef struct
+{
+	int deb;
+	int fin;
+	float* a;
+	float** b;
+	float* c;
+}donnees;
+
+void* MultVM(void* arg)
+{
+	int i,j;
+	donnees* ar = (donnees*)arg;
+	
+	for(i = ar->deb;i < ar->fin;i++)
+	{
+		for(j=0;j<w;j++)
+		{
+			ar->c[i] += (ar->a[j])*(ar->b[i][j]);
+		}
+	}
+	return NULL;
+}
 
 int main()
 {
@@ -18,7 +45,12 @@ int main()
 	}
 	float* c = malloc(h*sizeof(float));
 	
-	pthread_t* pthr = malloc(h*sizeof(pthread_t));
+	if(h>=PTHREAD_THREADS_MAX)
+		k = PTHREAD_THREADS_MAX-1;
+	else
+		k = h;
+		
+	pthread_t pthr[h];
 	
 	srand(time(NULL));
 	
@@ -32,7 +64,9 @@ int main()
 		c[i] = 0;
 	}
 	
-	
+	//----------------------------
+    
+    //----------------------------
 	
 	deb=clock();
 	
@@ -45,19 +79,53 @@ int main()
 	}
     
     fin=clock();
+    
     printf("nothing : %fs\n", (double) (fin-deb)/CLOCKS_PER_SEC);
     
+    //----------------------------
     
-    for(k=0;k<h;k++)
+    //----------------------------
+    
+    donnees* info = malloc(PTHREAD_THREADS_MAX*sizeof(donnees));
+    donnees info_perso;
+    info_perso.deb=(h/k)*(k-1);
+    info_perso.fin=h;
+    info_perso.a=a;
+    info_perso.b=b;
+    info_perso.c=c;
+    
+    for(k=1;k<PTHREAD_THREADS_MAX;k++)
+    //for(k=1;k<2;k++)
     {
+		for(i=0;i<h;i++)
+			c[i] = 0;
+		
 		deb=clock();
 		
+		for(i=0;i<k;i++)
+		{
+			info[i].deb = (h/k)*i;
+			info[i].fin = (h/k)*(i+1) - 1;
+			info[i].a = a;
+			info[i].b = b;
+			info[i].c = c;
+			
+			pthread_create(&pthr[i], NULL, MultVM, &info[i]);
+		}
+		MultVM(&info_perso);
 		
+		for(i=0;i<k;i++)
+		{
+			pthread_join(pthr[i], NULL);
+		}
 		
 		fin=clock();
 		printf("%d thread : %fs\n", k, (double) (fin-deb)/CLOCKS_PER_SEC);
 	}
     
+    //----------------------------
+    
+    //----------------------------
     
     return 0;
 }
