6439db124d6168e6b33623dadfee7b09c2dbb76f
mortexsa <slimani-arezki@hotmail.com>
2018-04-09 17:24:45 +0200

    creation de la premiere interface juste pour tester le rendu, c'est deguelasse x)

--- testInterface/exo1.c
+++ testInterface/exo1.c
@@ -1,32 +1,42 @@
-#include <stdlib.h>
-#include <string.h>
 #include <gtk/gtk.h>
-
-
-void mafonction(GtkWidget *widget, gpointer user_data)
+#include <stdio.h>
+ 
+int main(int argc, char* argv[])
 {
-	int i;
-	const gchar * mot = gtk_window_get_title(GTK_WINDOW(widget));
-	for(i=strlen(mot); i>=0; i--){
-		printf("%c", mot[i]);
-	}
-	printf("\n");
-    gtk_main_quit();
-}
-
-
-int main(int argc,char **argv)
-{
-	GtkWidget* Fenetre = NULL;
-	char * titre = "Bonjour din yemmak";
-	gtk_init(&argc, &argv);
-
-    Fenetre = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(Fenetre), titre);
-    gtk_window_set_default_size(GTK_WINDOW(Fenetre), 300, 100);
-    gtk_widget_show_all(Fenetre);
-    g_signal_connect(G_OBJECT(Fenetre), "delete-event", G_CALLBACK(mafonction), NULL);
-    
+    GtkWidget* window;
+    GtkWidget* box;
+    GtkWidget *scrollbar;
+    int i;
+     
+    gtk_init(&argc, &argv);
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_default_size(GTK_WINDOW(window), 320, 200);
+    gtk_window_set_title(GTK_WINDOW(window), "GtkScrolledWindow");
+    g_signal_connect(G_OBJECT(window),"destroy",G_CALLBACK(gtk_main_quit),0);
+     
+    scrollbar = gtk_scrolled_window_new(NULL, NULL);
+    gtk_container_add(GTK_CONTAINER(window),scrollbar);
+     
+    box=gtk_vbox_new(FALSE,5);
+    gtk_scrolled_window_add_with_viewport(GTK_SCROLLED_WINDOW(scrollbar), box);
+     
+    gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrollbar), GTK_POLICY_NEVER, GTK_POLICY_ALWAYS);
+     
+    for(i = 1 ; i <= 10 ; ++i)
+    {
+        GtkWidget *label;
+        char texte[10];
+         
+        sprintf(texte, "Label %d", i);
+         
+        label = gtk_label_new(texte);
+         
+        gtk_box_pack_start(GTK_BOX(box), label, FALSE, FALSE, 5);
+    }
+     
+    gtk_widget_show_all(window);
+     
     gtk_main();
-	return EXIT_SUCCESS;
-}
+     
+    return 0;
+} 
