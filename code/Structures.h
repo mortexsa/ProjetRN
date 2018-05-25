6acac748a6af03267f37fd28d25d603b475268e7
pepnou <pepnou1@gmail.com>
2018-05-25 20:04:19 +0200

    doc

--- code/Structures.h
+++ code/Structures.h
@@ -14,60 +14,82 @@
 
 #define debug printf("line : %d in function : %s in file %s\n",__LINE__,__func__,__FILE__);
 
-//#define float float
-
+/**
+ * \struct Pixel
+ * \brief definie les composantes rgb d'un pixel
+ */
 typedef struct Pixel
 {
-	unsigned char r,g,b;
+	unsigned char r,g,b;/*!< composante couleur entre 0 et 255 */
 } Pixel;
 
+/**
+ * \struct Image
+ * \brief contient chaque pixel deffinissant une image ainsi que la taille de celle ci
+ */
 typedef struct Image
 {
-	int w,h;
-	Pixel* dat;
+	int w,h; /*!< taille de l'image */
+	Pixel* dat;/*!< tableau contenant les pixels */
 } Image;
 
+/**
+ * \struct COUCHE
+ * \brief decrit une couche d'un réseau de neurone
+ */
 struct COUCHE
 {
-	float* A;
-	float* B;
-	float** W;
+	float* A; /*!< vecteur des activation de la couche */
+	float* B; /*!< vecteur des biais de la couche */
+	float** W; /*!< matrice des poids de la couche */
 	
-	float* DELTA;     // Vecteur de modification des biais
-	float** DELTA_M;  // Matrice de modification des poids
+	float* DELTA; /*!< Vecteur de modification des biais */
+	float** DELTA_M; /*!< Matrice de modification des poids */
 	
-	int taille;
+	int taille; /*!< nombre de neurone de la couche */
 		
-	struct COUCHE* prec;
-	struct COUCHE* suiv;
+	struct COUCHE* prec; /*!< pointeur vers la couche précedente */
+	struct COUCHE* suiv; /*!< pointeur vers la couche suivante */
 };
 typedef struct COUCHE COUCHE;
 typedef COUCHE* Liste_Couche;
 
+/**
+ * \struct INFO_RN
+ * \brief contient les information concernant un réseau de neurones
+ */
 struct INFO_RN
 {
-	char** etiquettes;
-	char* nom;
-	char* date;
-	char* repertoire;
-	int reussite;
-	int echec;
-	int w,h; //w = largeur, h = hauteur
+	char** etiquettes; /*!< tableau de chaine de caractères contenant toutes les étiquettes des neurones de la derniere couche d'un réseau de neurones */
+	char* nom; /*!< nom du réseau de neurones */
+	char* date; /*!< date de création du réseau de neurones */
+	char* repertoire; /*!< repertoire ou se situent les exemples d'apprentissage */
+	int reussite; /*!< nombre d'image bien reconnue lors de l'apprentissage */
+	int echec; /*!< nombre d'image mal reconnue lors de l'apprentissage */
+	int w,h; /*!< taille des images acceptées par le réseau de neurones */ //w = largeur, h = hauteur
 };
 typedef struct INFO_RN INFO_RN;
 
+/**
+ * \struct RN
+ * \brief defini un réseau de neurones
+ */
 struct RN
 {
-	INFO_RN* info;
-	Liste_Couche couche_deb;
-	Liste_Couche couche_fin;
+	INFO_RN* info; /*!< information du réseau de neurones */
+	Liste_Couche couche_deb; /*!< pointeur vers la premiere couche du réseau de neurone */
+	Liste_Couche couche_fin; /*!< pointeur vers la derniere couche du réseau de neurone */
 };
 typedef struct RN RN;
 
+/**
+ * \struct Apprentissage
+ * \brief regroupe un couple données d'entré et sortie attendue necessaire lors de l'apprentissage
+ */
 struct Apprentissage
 {
-	Image* image;
-	char* etiquette;
+	Image* image; /*!< image correspondant a l'étiquette */
+	char* etiquette; /*!< étiquette correspondant a l'image */
 };
 typedef struct Apprentissage App;
 
